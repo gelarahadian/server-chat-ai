aa33a9fd2ebe73e4693860dde9a64443d7b42068
gelarahadian <gelar.rahadian.fajar@gmail.com>
2026-01-11 08:38:42 +0700

    For run in Vercel

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import logger from "./middlewares/logger";
 import { errorHandler } from "./middlewares/errorHandler";
 import routes from "./routes/routes";
+import connectDB from "./config/db";
 
 const app: express.Application = express();
 
@@ -19,9 +20,12 @@ app.use(logger);
 app.use(errorHandler);
 app.use(routes);
 
+connectDB();
+
 app.get("/", (req, res) => {
   res.send("TypeScript With Express");
 });
+
 
 export default app
 
